47fe16edfb2014b8165ae986f686cf7b772a1f1a
Chris Wein <chriswein12@yahoo.com>
2020-09-20 23:04:32 -0500

    added prompts to code

--- script.js
+++ script.js
@@ -1,5 +1,70 @@
 // Assignment code here
 
+var characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"]
+
+var numChar = function() {
+
+  var numberEntered = window.prompt("How many characters are needed for the password, please choose between 8 and 128."); 
+
+  numberEntered = parseInt(numberEntered);
+
+  if (numberEntered < 8 || numberEntered > 128 ) {
+    window.alert("Please pick a number of characters between 8 and 128");
+    numChar();
+    }
+    
+    console.log(numberEntered); 
+    return numberEntered; 
+  }
+
+var generatePassword = function() {
+ 
+  numChar();
+
+  var uppercaseVal = window.confirm("Press OK if password requires uppercase letters.");
+  var numberVal = window.confirm("Press OK if password require numbers.");
+  var specialCharVal = window.confirm("Does password require special characters.");
+
+  if (uppercaseVal) {
+    characters.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+  }
+
+  if (numberVal) {
+    characters.push("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+  }
+
+  if (specialCharVal) {
+    characters.push("!", "#", "&", "$", "%", "&", "*", "+", "@", "=", "?", "~")
+  }
+
+  
+
+
+}
+
+
+
+
+
+var numbers = []
+
+var requireNumbers = function() {
+
+}
+
+var requireLetters = function () {
+
+}
+
+
+
+
+
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
